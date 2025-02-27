b22a4763adcfaaf543240c6862639769e1ddda24
Felipe Silva <felipe-dev@outlook.com>
2025-02-26 23:32:03 -0300

    Mundando nome da pasta

--- src/main/kotlin/com/earthsea/ia_dev/controllers/ChatInteractionController.kt
+++ src/main/kotlin/com/earthsea/ia_dev/controllers/ChatInteractionController.kt
@@ -1,24 +1,34 @@
 package com.earthsea.ia_dev.controllers
 
-import com.earthsea.ia_dev.services.AIClient
+import com.earthsea.ia_dev.services.AIClientDeepSeek
 import kotlinx.coroutines.runBlocking
 import org.springframework.http.ResponseEntity
 import com.earthsea.ia_dev.forms.QuestionForm
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/chat")
 class  ChatInteractionController {
 
-    private val aiClient = AIClient()
+    private val AIClientDeepSeek = AIClientDeepSeek()
 
     @PostMapping("/generate")
     fun generateResponse(@RequestBody questionForm: QuestionForm): ResponseEntity<Any> {
         return try {
-            val response = runBlocking { aiClient.askQuestion(questionForm.question) }
+
+            val response = runBlocking {
+                AIClientDeepSeek.askQuestion(questionForm.question)
+            }
+
             ResponseEntity.ok(mapOf("response" to response))
+
         } catch (e: Exception) {
+
             ResponseEntity.status(500).body(mapOf("error" to "Erro ao processar resposta: ${e.message}"))
+
         }
     }
 }
